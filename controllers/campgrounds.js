f403775e3e7f9369f2c5673bf24348a771a6458f
Divija Morishetty <divijamorishetty@Divijas-MacBook-Air.local>
2024-06-30 13:55:39 -0400

    Add Map Box Functionality

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
 const { isLoggedIn } = require('../middleware');
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken : mapBoxToken});
 const { cloudinary } = require("../cloudinary");
 
 
@@ -12,9 +15,14 @@ module.exports.renderNewForm = (req, res) => {
     res.render('campgrounds/new');
 }
 
-module.exports.createCampground = async (req, res) => {
+module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground);
-    campground.images=req.files.map(f=>({url: f.path, filename: f.filename}));
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
     console.log(campground);
--- seeds/index.js
+++ seeds/index.js
@@ -27,6 +27,13 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+              type: "Point",
+              coordinates: [
+                cities[random1000].longitude,
+                cities[random1000].latitude,
+            ]
+          },
             images: [
                 {
                   url: 'https://res.cloudinary.com/dixoxtrut/image/upload/v1718301486/Yelpcamp/jcbzzj0hqkc5br0tgwcj.png',
