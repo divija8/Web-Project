50c91c8160d520827cd7edf19b72428e1f19f8e0
Divija Morishetty <divijamorishetty@Divijas-MacBook-Air.local>
2024-06-30 20:27:22 -0400

    Add Cluster Map Properties

--- models/campground.js
+++ models/campground.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const Review = require('./review')
-
-
+const Schema = mongoose.Schema;
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
 const ImageSchema = new Schema({
     url: String,
     filename: String
 });
-
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
+
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -39,7 +39,14 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
@@ -50,6 +57,4 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
         })
     }
 })
-
-
 module.exports = mongoose.model('Campground', CampgroundSchema);
